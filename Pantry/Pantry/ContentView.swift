79b640b172edfe73bafdbe81b82883593b115fe1
Yaniv Amiri <yanivam123@gmail.com>
2020-10-09 13:11:43 -0400

    Created a project on xCode, going to make a branch

---
+++ Pantry/Pantry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pantry
+//
+//  Created by Yaniv Amiri on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pantry/Pantry/PantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryApp.swift
+//  Pantry
+//
+//  Created by Yaniv Amiri on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct PantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
